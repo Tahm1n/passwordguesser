54f1af37f94c8afe8f2e101d870556a7e97cb3ec
Tahmin Ahmed <theosphice97@gmail.com>
2017-03-15 20:21:59 +0000

    PasswordGuesser

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    
+     $("button").click(function() {
+      var guess = $("input:text").val();
+      var answer="crocodile";
+      
+      if (guess===answer) {
+        $(".answer").append("Correct Answer Pal!! ");  
+      }else{ 
+          
+      }
+
+        
+        
+        
+    });
+    
+    
+    });
+   
